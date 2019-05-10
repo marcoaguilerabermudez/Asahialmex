aafacaa863886c27153affea5bb602c85a9b39e6
Luis Limón <luiisalexlimon@hotmail.com>
2019-05-10 11:46:55 -0500

    Se agregarón las bibliotecas de clase, "Clases", "Datos", "Negocio" y "Presentacion", a demás se le agrefo un formulario prueba de Login ("FrmLogin")

---
+++ AsahiApp/AsahiApp/Clases/Productos.vb
@@ -0,0 +1,11 @@
+﻿Public Class Productos
+    Private idProducto_ As Integer
+    Public Property IdProducto() As Integer
+        Get
+            Return idProducto_
+        End Get
+        Set(ByVal value As Integer)
+            idProducto_ = value
+        End Set
+    End Property
+End Class
