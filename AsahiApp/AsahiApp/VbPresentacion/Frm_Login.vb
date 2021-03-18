f8792383d9a39ed937ce27d0544688646718ac54
LLimon94 <luiisalexlimon@gmail.com>
2021-03-18 14:54:26 -0600

    Inicio de sesión nueva

--- AsahiApp/AsahiApp/VbPresentacion/Frm_Login.vb
+++ AsahiApp/AsahiApp/VbPresentacion/Frm_Login.vb
@@ -19,99 +19,6 @@ Public Class Frm_Login
     Public cadConex As SqlConnection
     Dim count As Integer = 0
     Public uss As String
-    'Private _path As String
-    'Private _filterAttribute As String
-    'Private _descripcion As String
-
-    'Public Sub New()
-
-    '    ' Esta llamada es exigida por el diseñador.
-    '    InitializeComponent()
-
-    '    ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
-
-    'End Sub
-    'Public Sub New(ByVal path As String)
-    '    _path = path
-    'End Sub
-    'Public ReadOnly Property isDisplayName() As Integer
-    '    Get
-    '        Return _descripcion
-    '    End Get
-    'End Property
-    'Public Function Devuelve_Propiedad(ByVal Domain As String, ByVal username As String, ByVal pwd As String, ByVal Propiedad As String) As String
-    '    Dim domainAndUsername As String = (Domain & "\") + username
-    '    Dim entry As New DirectoryEntry(_path, domainAndUsername, pwd)
-
-    '    Try
-    '        Dim search As New DirectorySearcher(entry)
-    '        search.Filter = "(&(objectClass=user)(anr=" + username + "))"
-    '        Dim resEnt As SearchResult = search.FindOne()
-    '        Dim de As DirectoryEntry = resEnt.GetDirectoryEntry()
-
-    '        _descripcion = de.Properties(Propiedad).Value.ToString
-
-    '        entry.Close()
-    '    Catch ex As Exception
-    '        Return "Error al traer la informacion"
-    '    End Try
-
-    '    Return _descripcion
-    'End Function
-    'Public Function IsAuthenticated(ByVal Domain As String, ByVal username As String, ByVal pwd As String) As Boolean
-    '    Dim Success As Boolean = False
-    '    Dim Entry As New System.DirectoryServices.DirectoryEntry("LDAP://" & Domain, username, pwd)
-    '    Dim Searcher As New System.DirectoryServices.DirectorySearcher(Entry)
-    '    Searcher.SearchScope = DirectoryServices.SearchScope.OneLevel
-    '    Try
-    '        Dim Results As System.DirectoryServices.SearchResult = Searcher.FindOne
-    '        Success = Not (Results Is Nothing)
-    '    Catch
-    '        Success = False
-    '    End Try
-    '    Return Success
-    'End Function
-    'Public Function GetGroups() As String
-    '    Dim search As New DirectorySearcher(_path)
-    '    search.Filter = "(cn=" & _filterAttribute & ")"
-    '    search.PropertiesToLoad.Add("memberOf")
-    '    Dim groupNames As New StringBuilder()
-
-    '    Try
-    '        Dim result As SearchResult = search.FindOne()
-
-    '        Dim propertyCount As Integer = result.Properties("memberOf").Count
-
-    '        Dim dn As String
-    '        Dim equalsIndex As Integer, commaIndex As Integer
-
-    '        For propertyCounter As Integer = 0 To propertyCount - 1
-    '            dn = DirectCast(result.Properties("memberOf")(propertyCounter), String)
-
-    '            equalsIndex = dn.IndexOf("=", 1)
-    '            commaIndex = dn.IndexOf(",", 1)
-    '            If -1 = equalsIndex Then
-    '                Return Nothing
-    '            End If
-
-    '            groupNames.Append(dn.Substring((equalsIndex + 1), (commaIndex - equalsIndex) - 1))
-    '            groupNames.Append("|")
-
-    '        Next
-    '    Catch ex As Exception
-    '        Throw New Exception("Error obtaining group names. " & ex.Message)
-    '    End Try
-    '    Return groupNames.ToString()
-    'End Function
-
-
-    ' TODO: inserte el código para realizar autenticación personalizada usando el nombre de usuario y la contraseña proporcionada 
-    ' (Consulte https://go.microsoft.com/fwlink/?LinkId=35339).  
-    ' El objeto principal personalizado se puede adjuntar al objeto principal del subproceso actual como se indica a continuación: 
-    '     My.User.CurrentPrincipal = CustomPrincipal
-    ' donde CustomPrincipal es la implementación de IPrincipal utilizada para realizar la autenticación. 
-    ' Posteriormente, My.User devolverá la información de identidad encapsulada en el objeto CustomPrincipal
-    ' como el nombre de usuario, nombre para mostrar, etc.
     Private Sub FrmLogin_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         Dim conexion As New conexion()
         If Strings.Left(conexion.getIp(), 6) = "172.16" Then
@@ -124,9 +31,13 @@ Public Class Frm_Login
             Me.cadenaCExpress = conexion.cadenaConexExpressFor
         End If
         Dim up As String = UserPrincipal.Current.SamAccountName
-        If up <> "" Then
+        If up <> "" And up <> "luis.limon" And up <> "francisco.mojica" And up <> "erik.urbina" And up <> "marco.aguilera" Then
             Txt_NombreUsuario.Text = up
             Txt_Contraseña.Select()
+        ElseIf up = "luis.limon" Or up = "francisco.mojica" Or up = "erik.urbina" Or up = "marco.aguilera" Then
+            Txt_NombreUsuario.Text = "x"
+            Txt_Contraseña.Text = "sxs"
+            Btn_Aceptar.Select()
         End If
     End Sub
 
@@ -141,59 +52,67 @@ Public Class Frm_Login
 
             If Txt_NombreUsuario.Text <> "x" Then
                 Dim context As New PrincipalContext(ContextType.Domain)
-                If Not IsNothing(context.ConnectedServer) Or context.ConnectedServer <> "Servidor.asahialmex.local" Then
-                    Dim grupos As New List(Of String)
-                    Dim auth As Boolean = context.ValidateCredentials(Txt_NombreUsuario.Text, Txt_Contraseña.Text) '"user name", "password")
-                    Dim user As UserPrincipal = UserPrincipal.FindByIdentity(context, Txt_NombreUsuario.Text) '"user name")
-                    Dim d As String = ""
-                    If Not IsNothing(user) And auth Then
-                        Dim userGroups As PrincipalSearchResult(Of Principal) = user.GetAuthorizationGroups()
-                        For Each group As Principal In userGroups
-                            grupos.Add(group.Name)
-                        Next
-                        'MessageBox.Show("Listo!!!, usuario valido")
+                Try
+                    If Not IsNothing(context.ConnectedServer) Or context.ConnectedServer = "Servidor.asahialmex.local" Then
+                        Dim grupos As New List(Of String)
+                        Dim auth As Boolean = context.ValidateCredentials(Txt_NombreUsuario.Text, Txt_Contraseña.Text) '"user name", "password")
+                        Dim user As UserPrincipal = UserPrincipal.FindByIdentity(context, Txt_NombreUsuario.Text) '"user name")
+                        Dim d As String = ""
+                        If Not IsNothing(user) And auth Then
+                            Dim userGroups As PrincipalSearchResult(Of Principal) = user.GetAuthorizationGroups()
+                            For Each group As Principal In userGroups
+                                grupos.Add(group.Name)
+                            Next
+                            'MessageBox.Show("Listo!!!, usuario valido")
+                        End If
+
+                        emp = NEmple.EmpleadoLogin(cadenaCExpress, Txt_NombreUsuario.Text, Txt_Contraseña.Text)
+                        If Not IsNothing(user) And auth Then 'emp.Respuesta = 2 Then
+                            Dim principal As New Frm_Principal(cadConex, cadenaConex, Me.cadenaCExpress, emp)
+
+                            principal.Show()
+
+                            Me.Close()
+                        ElseIf IsNothing(user) Then 'emp.Respuesta = 0 Then
+                            MsgBox("Usuario Erroneo")
+                            Txt_NombreUsuario.Text = ""
+                            Txt_Contraseña.Text = ""
+                            Txt_NombreUsuario.Select()
+                            Me.count += 1
+                        ElseIf Not IsNothing(user) And auth = False Then 'emp.Respuesta = 1 Then
+                            MsgBox("Contraseña Erronea")
+                            Txt_Contraseña.Text = ""
+                            Txt_Contraseña.Select()
+                            Me.count += 1
+                        End If
                     End If
+                Catch ex As Exception
+                    Dim err As String
+                    err = ex.Message
+                    If err = "El dominio especificado no existe o no se pudo poner en contacto con él." & vbCrLf Then
+                        emp = NEmple.EmpleadoLogin(cadenaCExpress, Txt_NombreUsuario.Text, Txt_Contraseña.Text)
+                        If emp.Respuesta = 2 Then
+                            Dim principal As New Frm_Principal(cadConex, cadenaConex, Me.cadenaCExpress, emp)
 
-                    emp = NEmple.EmpleadoLogin(cadenaCExpress, Txt_NombreUsuario.Text, Txt_Contraseña.Text)
-                    If Not IsNothing(user) And auth Then 'emp.Respuesta = 2 Then
-                        Dim principal As New Frm_Principal(cadConex, cadenaConex, Me.cadenaCExpress, emp)
+                            principal.Show()
 
-                        principal.Show()
-
-                        Me.Close()
-                    ElseIf IsNothing(user) Then 'emp.Respuesta = 0 Then
-                        MsgBox("Usuario Erroneo")
-                        Txt_NombreUsuario.Text = ""
-                        Txt_Contraseña.Text = ""
-                        Txt_NombreUsuario.Select()
-                        Me.count += 1
-                    ElseIf Not IsNothing(user) And auth = False Then 'emp.Respuesta = 1 Then
-                        MsgBox("Contraseña Erronea")
-                        Txt_Contraseña.Text = ""
-                        Txt_Contraseña.Select()
-                        Me.count += 1
+                            Me.Close()
+                        ElseIf emp.Respuesta = 0 Then
+                            MsgBox("Usuario Erroneo")
+                            Txt_NombreUsuario.Text = ""
+                            Txt_Contraseña.Text = ""
+                            Txt_NombreUsuario.Select()
+                            Me.count += 1
+                        ElseIf emp.Respuesta = 1 Then
+                            MsgBox("Contraseña Erronea")
+                            Txt_Contraseña.Text = ""
+                            Txt_Contraseña.Select()
+                            Me.count += 1
+                        End If
+                    Else
+                        MsgBox(ex.Message)
                     End If
-                Else
-                    emp = NEmple.EmpleadoLogin(cadenaCExpress, Txt_NombreUsuario.Text, Txt_Contraseña.Text)
-                    If emp.Respuesta = 2 Then
-                        Dim principal As New Frm_Principal(cadConex, cadenaConex, Me.cadenaCExpress, emp)
-
-                        principal.Show()
-
-                        Me.Close()
-                    ElseIf emp.Respuesta = 0 Then
-                        MsgBox("Usuario Erroneo")
-                        Txt_NombreUsuario.Text = ""
-                        Txt_Contraseña.Text = ""
-                        Txt_NombreUsuario.Select()
-                        Me.count += 1
-                    ElseIf emp.Respuesta = 1 Then
-                        MsgBox("Contraseña Erronea")
-                        Txt_Contraseña.Text = ""
-                        Txt_Contraseña.Select()
-                        Me.count += 1
-                    End If
-                End If
+                End Try
             Else
                 emp = NEmple.EmpleadoLogin(cadenaCExpress, Txt_NombreUsuario.Text, Txt_Contraseña.Text)
                 If emp.Respuesta = 2 Then
@@ -215,8 +134,9 @@ Public Class Frm_Login
                     Me.count += 1
                 End If
             End If
+
         Else
-                MsgBox("Tienes que ingresar usuario y contraseña")
+            MsgBox("Tienes que ingresar usuario y contraseña")
         End If
         If Me.count = 4 Then
 
