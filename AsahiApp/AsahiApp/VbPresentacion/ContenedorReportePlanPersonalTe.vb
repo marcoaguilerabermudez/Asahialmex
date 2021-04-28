8b3696a2cb3d7edc55a46c9f3a26e877917d4f40
LLimon94 <luiisalexlimon@gmail.com>
2021-04-28 16:52:21 -0500

    Cambios Erik

--- AsahiApp/AsahiApp/VbPresentacion/ContenedorReportePlanPersonalTe.vb
+++ AsahiApp/AsahiApp/VbPresentacion/ContenedorReportePlanPersonalTe.vb
@@ -5,6 +5,7 @@
     Public año As Integer
     Private Sub ContenedorReportePlanPersonalTe_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: esta línea de código carga datos en la tabla 'DataSet19.Sp_reportePlanPersonalTiempo' Puede moverla o quitarla según sea necesario.
+        DataSet19.EnforceConstraints = False
         Me.Sp_reportePlanPersonalTiempoTableAdapter.Fill(Me.DataSet19.Sp_reportePlanPersonalTiempo, depto, mes, año)
 
         Me.ReportViewer1.RefreshReport()
